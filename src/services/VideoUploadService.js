5e2853e0c13c824c5457700d133e48108ac52cd7
Romain Baumier <baumier.romain@gmail.com>
2017-06-30 14:17:26 +0200

    feat(production mode)

--- src/services/VideoUploadService.js
+++ src/services/VideoUploadService.js
@@ -7,24 +7,24 @@ module.exports = (VimeoService) => {
 
     // Fonction d'upload (défini par Vimeo)
     upload(path, onResponse, onProgress) {
-      let uploadedSize = 0;
-      let fileSize = 100;
-      var interval = setInterval(function () {
-        uploadedSize += 5;
-        onProgress(uploadedSize, fileSize);
-      }, 1000);
+      // let uploadedSize = 0;
+      // let fileSize = 100;
+      // var interval = setInterval(function () {
+      //   uploadedSize += 5;
+      //   onProgress(uploadedSize, fileSize);
+      // }, 1000);
 
-      setTimeout(function () {
-        clearInterval(interval);
-        onResponse(null, 'ici');
-      }, 20000);
+      // setTimeout(function () {
+      //   clearInterval(interval);
+      //   onResponse(null, 'ici');
+      // }, 20000);
 
-      // VimeoService.upload(path, (err, body, status, headers) => {
-      //   if(err) {
-      //     return onResponse(err);
-      //   }
-      //   onResponse(null, headers.location);
-      // }, onProgress);
+      VimeoService.upload(path, (err, body, status, headers) => {
+        if(err) {
+          return onResponse(err);
+        }
+        onResponse(null, headers.location);
+      }, onProgress);
     }
   };
 };
