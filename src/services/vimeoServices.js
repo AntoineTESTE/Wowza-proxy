a33bc81925409ec81bf8151a118a8b0d345082ae
Romain Baumier <baumier.romain@gmail.com>
2017-08-08 11:56:51 +0200

    fix(vimeoservice)

--- src/services/vimeoServices.js
+++ src/services/vimeoServices.js
@@ -7,12 +7,12 @@ const lib = new Vimeo('', '', access_token);
 module.exports = () => {
   return {
     upload(filePath, onResponse, onProgress) {
-      onProgress(0, 0);
-      setTimeout(() => {
-        onResponse(null, {}, {}, { location: '/videos/219717161' });
-      }, 10000);
+      // onProgress(0, 0);
+      // setTimeout(() => {
+      //   onResponse(null, {}, {}, { location: '/videos/219717161' });
+      // }, 10000);
 
-      // lib.streamingUpload(filePath, onResponse, onProgress);
+      lib.streamingUpload(filePath, onResponse, onProgress);
     }
   }
 }
